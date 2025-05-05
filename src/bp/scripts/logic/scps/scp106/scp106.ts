ab024be09648739640936db90c40e1fe04453e8d
LuckedCoronet45 <lc45@lc-studios.net>
2025-05-05 09:57:31 +0900

    update

--- src/bp/scripts/logic/scps/scp106/scp106.ts
+++ src/bp/scripts/logic/scps/scp106/scp106.ts
@@ -1,6 +1,6 @@
 import * as mc from "@minecraft/server";
 import * as vec3 from "@lib/utils/vec3";
-import { SCP106_ENTITY_TYPE_ID, SCP106_STATE } from "./shared";
+import { getState, SCP106_ENTITY_TYPE_ID, SCP106_STATE } from "./shared";
 
 mc.system.afterEvents.scriptEventReceive.subscribe(
 	(event) => {
@@ -22,4 +22,25 @@ mc.system.afterEvents.scriptEventReceive.subscribe(
 );
 
 /** This function should be called for each SCP-106 entity every 0.5 seconds. */
-function onUpdate(scp106: mc.Entity): void {}
+function onUpdate(scp106: mc.Entity): void {
+	const state = getState(scp106);
+
+	if (state === SCP106_STATE.hidden) return;
+
+	// Give wither effect (corrosion) to entities in contact
+	const entitiesInContact = scp106.dimension.getEntities({
+		closest: 20,
+		maxDistance: 1.9,
+		location: vec3.add(scp106.location, vec3.UP),
+		excludeTypes: [SCP106_ENTITY_TYPE_ID],
+	});
+	for (const entity of entitiesInContact) {
+		entity.addEffect("wither", 60, { amplifier: 1 });
+	}
+
+	if (state === SCP106_STATE.default) {
+		onUpdateDefaultState(scp106);
+	}
+}
+
+function onUpdateDefaultState(scp106: mc.Entity): void {}
