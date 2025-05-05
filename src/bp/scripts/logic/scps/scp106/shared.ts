5dd4d558ceef8d4ebd0478d61a825a24f9b9fc85
LuckedCoronet45 <lc45@lc-studios.net>
2025-05-05 09:31:35 +0900

    update

--- src/bp/scripts/logic/scps/scp106/shared.ts
+++ src/bp/scripts/logic/scps/scp106/shared.ts
@@ -1,3 +1,5 @@
+import * as mc from "@minecraft/server";
+
 export const SCP106_ENTITY_TYPE_ID = "lc:scpdy_scp106";
 
 export const SCP106_STATE = {
@@ -10,3 +12,11 @@ export const SCP106_STATE = {
 	throwingLeft: 60,
 	retreating: 70,
 } as const;
+
+export function getState(scp106: mc.Entity): number {
+	return Number(scp106.getProperty("lc:state"));
+}
+
+export function setState(scp106: mc.Entity, value: number): void {
+	scp106.setProperty("lc:state", value);
+}
