edfd55eea1d1f767cee16d2af947bc4a71673c00
LuckedCoronet45 <lc45@lc-studios.net>
2025-05-05 09:37:23 +0900

    update

--- src/bp/scripts/logic/scps/scp106/scp106.ts
+++ src/bp/scripts/logic/scps/scp106/scp106.ts
@@ -1,3 +1,21 @@
 import * as mc from "@minecraft/server";
 import * as vec3 from "@lib/utils/vec3";
 import { SCP106_ENTITY_TYPE_ID, SCP106_STATE } from "./shared";
+
+mc.system.afterEvents.scriptEventReceive.subscribe(
+	(event) => {
+		if (!event.sourceEntity) return;
+		if (!event.sourceEntity.isValid) return;
+		if (event.sourceEntity.typeId !== SCP106_ENTITY_TYPE_ID) return;
+
+		const scp106 = event.sourceEntity;
+
+		switch (event.id) {
+			case "":
+				break;
+		}
+	},
+	{
+		namespaces: ["scpdy_scp106"],
+	},
+);
