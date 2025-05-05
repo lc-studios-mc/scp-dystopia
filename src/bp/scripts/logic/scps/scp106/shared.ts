d432c8fa2b38fcba2773a3215fe951ff3cf64ac8
LuckedCoronet45 <lc45@lc-studios.net>
2025-05-05 09:21:05 +0900

    update

--- src/bp/scripts/logic/scps/scp106/shared.ts
+++ src/bp/scripts/logic/scps/scp106/shared.ts
@@ -1,1 +1,12 @@
 export const SCP106_ENTITY_TYPE_ID = "lc:scpdy_scp106";
+
+export const SCP106_STATE = {
+	default: 0,
+	diving: 10,
+	hidden: 20,
+	appearingSlow: 30,
+	appearingFast: 40,
+	throwingRight: 50,
+	throwingLeft: 60,
+	retreating: 70,
+} as const;
